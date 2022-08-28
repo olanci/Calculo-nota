2fe0c125c2c42e30a711d6b60fea72a40933ca8e
Olanci <olanci@gmail.com>
2022-08-27 21:48:35 -0300

    nota

---
+++ src/Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora{
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        int nota1=2;
+        int nota2=2;
+        int nota3=2;
+        int nota;
+        nota=nota1+nota2+nota3;
+
+
+       
+       System.out.println(nota1+nota2+nota3/3);
+
+       
+
+       if (nota>=8){ 
+        System.out.println("Aprovado");}
+       else{
+        System.out.println("Reprovado");}
+
+       }
+    }
